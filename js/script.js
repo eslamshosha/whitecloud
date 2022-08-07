dd1efda7496d0619028a4ced768af15d23fe985a
eslam shosha <you@example.comeslamshosha22.es@gmail.com>
2022-08-07 13:56:05 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let hint = document.querySelector(".preloader");
+window.onload = function () {
+  //hide the preloader
+  setTimeout(function () {
+    hint.style.display = "none";
+  }, 700);
+};
+$(document).ready(function () {
+
+});
